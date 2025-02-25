5458988669a018bf47ca36be08bcec72052c904e
Philip Mattha <philip@mattha.net>
2025-02-25 12:34:18 +0100

    init

---
+++ src/utils/supabase/client.ts
@@ -0,0 +1,14 @@
+import { createClient } from "@supabase/supabase-js";
+import { Database } from "./database.types";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+export const supabaseClient = () => {
+  if (!supabaseUrl) throw new Error("No Supabase URL defined.");
+  if (!supabaseAnonKey) throw new Error("No Supabase Anon key defined.");
+  return createClient<Database>(supabaseUrl, supabaseAnonKey);
+};

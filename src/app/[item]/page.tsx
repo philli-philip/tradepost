a60b85b5fff17f7207b097846396ba2075b85b14
Philip Mattha <philip@mattha.net>
2025-02-26 10:00:21 +0100

    Error state for unsupported items

--- src/app/[item]/page.tsx
+++ src/app/[item]/page.tsx
@@ -12,9 +12,16 @@ export default async function Page({
   const itemId = (await params).item;
   const item = await fetchItem(itemId);
 
+  const isValidItem = allowedItems[itemId] !== undefined;
+  if (!isValidItem) {
+    return <div>Item not yet supported by the Trade post</div>;
+  }
+
   return (
     <div>
-      <h1>{allowedItems[itemId].name ?? itemId}</h1>
+      <h1 className="font-bold text-3xl tracking-tight pb-4">
+        {allowedItems[itemId].name ?? itemId}
+      </h1>
       <div className="grid grid-cols-2 gap-4">
         <StaticFacts item={itemId} />
         <MarketFacts marketData={item} />

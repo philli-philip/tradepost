3f52ae27211f9b68a22fb801e599a3d1ff0fb14b
Philip Mattha <philip@mattha.net>
2025-02-27 21:14:09 +0100

    add revalidation for page to improve data freshness

--- src/app/[item]/page.tsx
+++ src/app/[item]/page.tsx
@@ -8,6 +8,8 @@ import { Chart } from "./Chart";
 import Link from "next/link";
 import { ArrowLeft } from "lucide-react";
 
+export const revalidate = 60;
+
 export default async function Page({
   params,
   searchParams,

46be56ce5fc1f3f7d5c7cc4cf8581e30922ebd3f
Philip Mattha <philip@mattha.net>
2025-02-27 20:41:24 +0100

    add lucide-react for icon support and enhance page layout with back navigation

--- src/app/[item]/page.tsx
+++ src/app/[item]/page.tsx
@@ -5,6 +5,8 @@ import MarketFacts from "./marketFacts";
 import { Period } from "@/utils/types/idleClanApiTypes";
 import DateSelector from "./DateSelector";
 import { Chart } from "./Chart";
+import Link from "next/link";
+import { ArrowLeft } from "lucide-react";
 
 export default async function Page({
   params,
@@ -25,9 +27,18 @@ export default async function Page({
 
   return (
     <div className="flex flex-col gap-6">
-      <h1 className="font-bold text-3xl tracking-tight">
-        {allowedItems[itemId].name ?? itemId}
-      </h1>
+      <div className="flex flex-row-reverse justify-end gap-2">
+        <h1 className="font-bold text-2xl tracking-tight">
+          {allowedItems[itemId].name ?? itemId}
+        </h1>
+        <Link
+          href="/"
+          className="size-8 rounded-lg p-[2px]"
+          aria-label="Back to homepage"
+        >
+          <ArrowLeft size={28} className="text-gray-500 hover:text-gray-800" />
+        </Link>
+      </div>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
         <StaticFacts item={itemId} />
         <MarketFacts marketData={item} />

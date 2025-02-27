35ba68f167146bbc599db0f53251c3fc69179fc9
Philip Mattha <philip@mattha.net>
2025-02-27 21:08:57 +0100

    move chart component

--- src/app/[item]/Chart.tsx
+++ src/app/[item]/Chart.tsx
@@ -13,7 +13,7 @@ import {
   ChartContainer,
   ChartTooltip,
   ChartTooltipContent,
-} from "@/components/ui/chart";
+} from "@/components/ui/chart/chart";
 import {
   compactDate,
   hourDateFormat,
